4619134ba23cc4ef9b8f3c288eab2efb108ca856
Sebastian Castañeda <seca3234@gmail.com>
2020-05-12 18:16:25 -0500

    ...

--- Restaurant.java
+++ Restaurant.java
@@ -3,6 +3,14 @@ public class Restaurant {
     public static void main(String[] args) {
         RestaurantFactoryProducer rsp = new RestaurantFactoryProducer();
         RestaurantAbstractFactory r1 = rsp.getRestaurantAbstractFactory(1);
+        RestaurantAbstractFactory r2 = rsp.getRestaurantAbstractFactory(2);
+
+
+        System.out.println( r1.getFastFood("hot dog"));
+        System.out.println( r2.getFastFood("hot dog"));
+
+        System.out.println( r1.getDrink("beer"));
+        System.out.println( r2.getDrink("beer"));
     }
     
 }
